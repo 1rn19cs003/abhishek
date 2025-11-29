d5e7418cd2a3337b3c433fe70bef46a4acf32a6d
Abhishek Jaiswal <abhicse003@gmai.com>
2025-11-29 21:24:38 +0530

    changes has been done in next config file

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,12 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+        pathname: "/**",
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        pathname: "/**",
       },
     ],
   },
